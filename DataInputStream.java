45e6481194fa6eb2754a015d0195ef6d6b4acd47
raszreen <130258260+raszreen@users.noreply.github.com>
2023-06-08 01:07:20 +0800

    Create DataInputStream.java

---
+++ DataInputStream.java
@@ -0,0 +1,81 @@
+package lab12;
+
+import java.io.FileOutputStream;
+
+/**
+ * This program demonstrate reading byte-based data as it intended type 
+ * it is using DataInputStream.
+ * 
+ * The data represent a 6 days reading of daily rainfall from station Simpang Ampat in Alor Gajah district
+ * 
+ * The program calculates the average 6 days reading of daily rainfall and display it on the console.
+ * 
+ * @author raszreen
+ *
+ */
+
+public class DataOutputStream {
+
+	public DataOutputStream(FileOutputStream fileOutputStream) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		
+		// 1. Declare output file 
+		String outFile = "rainFallSimpatAmpat.txt";
+		
+		// Data declaration
+		String days[] = {"2/6","3/6","4/6","5/6","6/6","7/6"};
+		double dailyRainfall[] = {0.0, 4.0, 1.0, 0.0, 6.0, 19.0};
+		
+		try {
+			
+			// 2. Create stream to read data
+			DataOutputStream dos = new DataOutputStream(new FileOutputStream(outFile));
+			
+			// Process data
+			for (int index = 0; index < days.length; index++) {
+				
+				// 3. Write data into data stream
+				dos.writeUTF(days[index]);
+				dos.writeDouble(dailyRainfall[index]);
+				
+				// 4. Flush for each writing
+				dos.flush();
+			}
+			
+			// 5. Close stream
+			dos.close();
+			
+		} catch (Exception ex) {
+			
+			ex.printStackTrace();
+		}
+		
+		// Indicate end of program - Could be successful
+		System.out.println("End of program. Check out " + outFile); 
+	}
+
+	private void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void flush() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void writeDouble(double d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void writeUTF(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
